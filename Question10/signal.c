09ebf5db6d8330b665f3079d0fb8334a2cd17f0d
2024eb03173-svg <2024eb03173@online.bits-pilani.ac.in>
2026-02-05 16:27:15 +0530

    Add files via upload

---
+++ Question10/signal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void handle_term(int sig) {
+    printf("Received SIGTERM\n");
+}
+
+void handle_int(int sig) {
+    printf("Received SIGINT\n");
+    exit(0);
+}
+
+int main() {
+
+    signal(SIGTERM, handle_term);
+    signal(SIGINT, handle_int);
+
+    if(fork()==0) {
+        sleep(5);
+        kill(getppid(), SIGTERM);
+        exit(0);
+    }
+
+    if(fork()==0) {
+        sleep(10);
+        kill(getppid(), SIGINT);
+        exit(0);
+    }
+
+    while(1) sleep(1);
+}
